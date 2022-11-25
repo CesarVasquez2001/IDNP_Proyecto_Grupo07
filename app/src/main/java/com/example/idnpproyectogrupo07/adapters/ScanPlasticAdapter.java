6c3107963948bb72fbb55d939271df142c8a56d4
Jairo Castillo <jcastillo@unsa.edu.pe>
2022-11-25 11:07:20 -0500

    Guardando recycling view y cosas

---
+++ app/src/main/java/com/example/idnpproyectogrupo07/adapters/ScanPlasticAdapter.java
@@ -0,0 +1,83 @@
+package com.example.idnpproyectogrupo07.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.idnpproyectogrupo07.R;
+import com.example.idnpproyectogrupo07.classes.ScanItemsPlastic;
+
+import java.util.ArrayList;
+
+public class ScanPlasticAdapter extends RecyclerView.Adapter<ScanPlasticAdapter.ViewHolder> implements View.OnClickListener {
+
+    LayoutInflater inflater;
+    ArrayList<ScanItemsPlastic> model;
+
+    //listener
+    private View.OnClickListener listener;
+
+
+    public ScanPlasticAdapter(Context context, ArrayList<ScanItemsPlastic> model){
+
+        this.inflater = LayoutInflater.from(context);
+        this.model = model;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.fragment_plastic_list, parent, false);
+        view.setOnClickListener(this); //onclick
+
+        return new ViewHolder(view);
+    }
+
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String nombre = model.get(position).getNombre();
+        String placeholder = model.get(position).getPlaceholder();
+        int imagen = model.get(position).getImagenid();
+        holder.nombres.setText(nombre);
+        holder.placeholder.setText(placeholder);
+        holder.imagen.setImageResource(imagen);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return model.size();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (listener!=null) listener.onClick(view);
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView nombres, placeholder;
+        ImageView imagen;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nombres=itemView.findViewById(R.id.nombre_plastic);
+            placeholder =itemView.findViewById(R.id.referencia_plastic);
+            imagen = itemView.findViewById(R.id.image_plastic);
+        }
+    }
+
+}
--- app/src/main/java/com/example/idnpproyectogrupo07/ui/scan/PlasticListFragment.java
+++ app/src/main/java/com/example/idnpproyectogrupo07/ui/scan/PlasticListFragment.java
@@ -3,12 +3,19 @@ package com.example.idnpproyectogrupo07.ui.scan;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.idnpproyectogrupo07.R;
+import com.example.idnpproyectogrupo07.adapters.ScanPlasticAdapter;
+import com.example.idnpproyectogrupo07.classes.ScanItemsPlastic;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,33 +24,18 @@ import com.example.idnpproyectogrupo07.R;
  */
 public class PlasticListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ScanPlasticAdapter scanPlasticAdapter;
+    RecyclerView recyclerViewItemsPlastic;
+    ArrayList<ScanItemsPlastic> listaPlastic;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public PlasticListFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlasticListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static PlasticListFragment newInstance(String param1, String param2) {
         PlasticListFragment fragment = new PlasticListFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +43,58 @@ public class PlasticListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plastic_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_plastic_list,container,false);
+        recyclerViewItemsPlastic = view.findViewById(R.id.recyclerViewPlastic);
+        listaPlastic = new ArrayList<>();
+        //cargarlista
+        cargarLista();
+        //mostrarlista
+        mostrarLista();
+        return view;
     }
+
+    /*botella, wrapper,packet,plastic bag
+    , plastic tub, plastic tube, blister,  pouch, cramshell ,plastic jug, spray bottle, pump bottle ,plastic jar ,
+    aplicator, plastic case, unknown plastic/*
+     */
+    public void cargarLista(){
+        listaPlastic.add(new ScanItemsPlastic("Botella de Plastico","e.g. agua, jugos, gaseosas, etc", R.drawable.plastic));
+        listaPlastic.add(new ScanItemsPlastic("Envoltorio de Plastico","e.g. dulces, caramelos, goma de mascar, etc ", R.drawable.wrapper));
+        listaPlastic.add(new ScanItemsPlastic("Bolsa de Plastico","e.g. supermercado, ......, ...., etc ", R.drawable.bag));
+        listaPlastic.add(new ScanItemsPlastic("Pote de Plastico","e.g. mantequilla, helados, nutela, etc ", R.drawable.tub));
+        listaPlastic.add(new ScanItemsPlastic("Tubo de Plastico","e.g. crema dental, crema de manos, etc ", R.drawable.pastadental));
+        listaPlastic.add(new ScanItemsPlastic("Paquete de Blisters","e.g. pastillas, juguetes , usbs, etc ", R.drawable.antihistamines));
+        listaPlastic.add(new ScanItemsPlastic("Funda de Plastico","e.g. comida de mascota, popcorn microondas, etc ", R.drawable.pouch));
+        listaPlastic.add(new ScanItemsPlastic("Taper de Plastico","e.g. fruta, ensaladas, comidas, etc ", R.drawable.cramshell));
+        listaPlastic.add(new ScanItemsPlastic("Galonera de Plastico","e.g. yogurt, limpiador, leche, etc ", R.drawable.galonera));
+        listaPlastic.add(new ScanItemsPlastic("Botella de Spray","e.g. desinfectante, limpiador de superficies, etc ", R.drawable.spray));
+        listaPlastic.add(new ScanItemsPlastic("Surtidor de Plastico","e.g. alcohol en gel, jabon, lociones, etc ", R.drawable.pump));
+        listaPlastic.add(new ScanItemsPlastic("Bote de Plastico","e.g. suplementos, nutella, etc ", R.drawable.potegrande));
+        listaPlastic.add(new ScanItemsPlastic("Aplicador de Plastico","e.g. desodorante, balsamo labial, etc ", R.drawable.deodorant));
+        listaPlastic.add(new ScanItemsPlastic("Caja de Plastico","e.g. videojuegos, DVD, etc ", R.drawable.game));
+        listaPlastic.add(new ScanItemsPlastic("Plastico Desconocido","e.g. Si no conoces el plastico pero puedes dar mas detalles del mismo, etc ", R.drawable.unknown));
+
+
+    }
+
+    public void mostrarLista(){
+        recyclerViewItemsPlastic.setLayoutManager(new LinearLayoutManager(getContext()));
+        scanPlasticAdapter = new ScanPlasticAdapter(getContext(), listaPlastic);
+        recyclerViewItemsPlastic.setAdapter(scanPlasticAdapter);;
+
+        scanPlasticAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = listaPlastic.get(recyclerViewItemsPlastic.getChildAdapterPosition(view)).getNombre();
+                Toast.makeText(getContext(), "Selecciono"+nombre, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
