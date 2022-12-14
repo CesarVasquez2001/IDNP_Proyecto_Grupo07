0fbdee2434ecef5b0f612d842fc15e6d75710fc2
CesarVasquez2001 <cvasqueza@unsa.edu.pe>
2022-12-14 01:35:19 -0500

    Tabla user completada con imagen de perfil

--- app/src/main/java/com/example/idnpproyectogrupo07/database/DBUser.java
+++ app/src/main/java/com/example/idnpproyectogrupo07/database/DBUser.java
@@ -4,14 +4,22 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Serializable;
 
 public class DBUser extends DBHelper {
 
     Context context;
     private SQLiteDatabase db;
+
+    private ByteArrayOutputStream byteArrayOutputStream;
+    private byte[] imageInBytes;
 
     public DBUser(@Nullable Context context) {
         super(context);
@@ -31,16 +39,25 @@ public class DBUser extends DBHelper {
         try {
 
             ContentValues values = new ContentValues();
+
+            Bitmap imageToStoreBitmap = user.getProfile_picture();
+            byteArrayOutputStream = new ByteArrayOutputStream();
+            imageToStoreBitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+            imageInBytes = byteArrayOutputStream.toByteArray();
+
+
             values.put("fullname", user.getFullname());
             values.put("email", user.getFullname());
             values.put("password", user.getPassword());
             values.put("gender", user.getGender());
-            values.put("date_of_birth", user.getDateOfBirth());
+            values.put("date_of_birth", user.getDate_of_birth());
+            // BLOB
+            values.put("profile_picture",imageInBytes);
 
             id_user = db.insert("t_user", null, values);
 
         } catch (Exception exception) {
-            Toast.makeText(context, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            //Toast.makeText(context, exception.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
         return id_user;
@@ -57,7 +74,11 @@ public class DBUser extends DBHelper {
                     user.setEmail(cursor.getString(2));
                     user.setPassword(cursor.getString(3));
                     user.setGender(cursor.getString(4));
-                    user.setDateOfBirth(cursor.getString(5));
+                    user.setDate_of_birth(cursor.getString(5));
+
+                    byte[] imageByte = cursor.getBlob(6);
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(imageByte,0,imageByte.length);
+                    user.setProfile_picture(bitmap);
 
                 }while (cursor.moveToNext());
             }
@@ -66,6 +87,7 @@ public class DBUser extends DBHelper {
         }
         return user;
     }
+
 
     public boolean checkEmail(String email) {
         SQLiteDatabase db = this.getWritableDatabase();
